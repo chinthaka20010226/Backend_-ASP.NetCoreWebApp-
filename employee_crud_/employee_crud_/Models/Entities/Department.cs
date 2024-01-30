1e5168a298905b513b762ae5eb3ff967f575e267
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 01:01:30 +0530

    Newly cretaed, Entity is Department and Setup the properties...

---
+++ employee_crud_/employee_crud_/Models/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace employee_crud_.Models.Entities
+{
+    public class Department
+    {
+        [Key, Column("Id")]
+        [DisplayName("Department Id")]
+        public int Id { get; set; }
+
+        [Column("Name"), MaxLength(50)]
+        [DisplayName("Department Name")]
+        public String Name { get; set; }
+    }
+}
