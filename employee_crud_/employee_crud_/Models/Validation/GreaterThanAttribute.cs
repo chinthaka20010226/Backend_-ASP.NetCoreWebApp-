5959d5e6d566db2c28f087d3f137dde0b3e80448
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 00:53:10 +0530

    this is a validation part of some column attributes in Employee table...

--- employee_crud_/employee_crud_/Models/Validation/GreaterThanAttribute.cs
+++ employee_crud_/employee_crud_/Models/Validation/GreaterThanAttribute.cs
@@ -1,7 +1,34 @@
-﻿namespace employee_crud_.Models.Validation
-{
-    public class GreaterThanAttribute
-    {
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace employee_crud_.Models.Validation
+{
+    public class GreaterThanAttribute : ValidationAttribute
+    {
+        private readonly int _lowerLimit;
+
+        public GreaterThanAttribute(int lowerLimit)
+        {
+            _lowerLimit = lowerLimit;
+        }
+
+        protected override ValidationResult IsValid(Object value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                if(value is int intValue)
+                {
+                    if(intValue <= _lowerLimit)
+                    {
+                        return new ValidationResult($"The field {validationContext.DisplayName} must be greater than {_lowerLimit}.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException($"The value provided is not an interger.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
