ff43280e4137b5a4d39c4f32096d5b1c6ad49970
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 00:13:46 +0530

    setup primary key in Employee Table...

--- employee_crud_/employee_crud_/Models/Entities/Employee.cs
+++ employee_crud_/employee_crud_/Models/Entities/Employee.cs
@@ -1,8 +1,16 @@
-﻿namespace employee_crud_.Models.Entities
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace employee_crud_.Models.Entities
 {
     public class Employee
     {
+        [Key, Column("Id")]
+        [DisplayName("Employee ID")]
         public int Id { get; set; }
+
+
         public String FNAme { get; set; }
         public String LName { get; set; }
         public String Email { get; set; }
