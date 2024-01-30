6af90356c3077310e1dd04aead4a42b72b9b6615
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 00:10:13 +0530

    Create Employee Entity in Models... and includes properties...

---
+++ employee_crud_/employee_crud_/Models/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace employee_crud_.Models.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String FNAme { get; set; }
+        public String LName { get; set; }
+        public String Email { get; set; }
+        public DateOnly Dob { get; set; }
+        public double Salary { get; set; }
+    }
+}
