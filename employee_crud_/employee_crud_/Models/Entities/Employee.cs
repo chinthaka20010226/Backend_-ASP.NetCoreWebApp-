9f3008244fc909c86cff009bb5ccce4c435850fe
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 00:26:54 +0530

    setup another column of employeee table...

--- employee_crud_/employee_crud_/Models/Entities/Employee.cs
+++ employee_crud_/employee_crud_/Models/Entities/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using 
 
 namespace employee_crud_.Models.Entities
 {
@@ -10,11 +11,24 @@ namespace employee_crud_.Models.Entities
         [DisplayName("Employee ID")]
         public int Id { get; set; }
 
-
+        [Column("FirstName"), MaxLength(20)]
+        [DisplayName("First Name")]
         public String FNAme { get; set; }
+
+        [Column("LastName"), MaxLength(20)]
+        [DisplayName("Last Name")]
         public String LName { get; set; }
+
+        [Column("Email"), MaxLength(30)]
+        [DisplayName("Email Address")]
         public String Email { get; set; }
+
+        [Column("DOB")]
+        [DisplayName("Date of Birth")]
         public DateOnly Dob { get; set; }
+
+        [Column("Salary")]
+        [DisplayName("Salary")]
         public double Salary { get; set; }
     }
 }
