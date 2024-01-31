ac39c073c3fe344e8149cae0c98a49df923d93f9
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 23:15:01 +0530

    Method 01 -> EmployeeRepository

---
+++ employee_crud_/employee_crud_/Repositories/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using employee_crud_.Data;
+using employee_crud_.Interfaces.Repositories;
+
+namespace employee_crud_.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly DataContext _dataContext;
+        EmployeeRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
