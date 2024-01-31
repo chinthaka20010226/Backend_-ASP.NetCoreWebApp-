9124cbf8af7799157d8e4c4ddc052a0cb622f9b3
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 22:07:19 +0530

    Now, DepartmentRepository Interface...

--- employee_crud_/employee_crud_/Interfaces/Repositories/IDepartmentRepository.cs
+++ employee_crud_/employee_crud_/Interfaces/Repositories/IDepartmentRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace employee_crud_.Interfaces.Repositories
+﻿using employee_crud_.Models.Entities;
+
+namespace employee_crud_.Interfaces.Repositories
 {
     public interface IDepartmentRepository
     {
+        Task<Department?> GetDepartmentById(int id);
+        Task<Department?> GetDepartmentByName(String name);
+        Task<IEnumerable<Department?>> GetDepartments();
+        void InsertDepartment(Department department);
+        void UpdateDepartment(Department department);
+        void DeleteDepartment(int id);
+
+        Task Save();
     }
 }
