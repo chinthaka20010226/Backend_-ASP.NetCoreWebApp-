b628652e925606791a4eace24d4f1362beda4597
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 23:08:25 +0530

    GetEmployeeById() in EmployeeService Method...

--- employee_crud_/employee_crud_/Services/EmployeeService.cs
+++ employee_crud_/employee_crud_/Services/EmployeeService.cs
@@ -1,9 +1,21 @@
 ﻿using employee_crud_.Interfaces.Repositories;
 using employee_crud_.Interfaces.Services;
+using employee_crud_.Models.Entities;
 
 namespace employee_crud_.Services
 {
-    public class EmployeeService(IEmployeeService employee) : IEmployeeService
+    public class EmployeeService(IEmployeeRepository employees) : IEmployeeService
     {
+        public async Task<Employee> GetEmployeeById(int id)
+        {
+            if(id <= 0)
+            {
+                throw new InvalidArgumentException("must bo grater than 0");
+            }
+
+            var employee = employees.GetEmployeeById(id);
+
+            return await (employee ?? throw new NotFoundException($"Employee with id {id} not found"));
+        }
     }
 }
---
+++ employee_crud_/employee_crud_/Services/InvalidArgumentException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace employee_crud_.Services
+{
+    [Serializable]
+    internal class InvalidArgumentException : Exception
+    {
+        public InvalidArgumentException()
+        {
+        }
+
+        public InvalidArgumentException(string? message) : base(message)
+        {
+        }
+
+        public InvalidArgumentException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidArgumentException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
