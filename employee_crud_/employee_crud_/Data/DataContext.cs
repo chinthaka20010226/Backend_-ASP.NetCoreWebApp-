a5bacae03e7d12a158523ce67903bfe72a3ca0fe
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 16:23:02 +0530

    Method 02 -> DbContext...

--- employee_crud_/employee_crud_/Data/DataContext.cs
+++ employee_crud_/employee_crud_/Data/DataContext.cs
@@ -3,13 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace employee_crud_.Data
 {
-    public class DataContext : DbContext
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-
-        }
-
         public DbSet<Employee> Employee { get; set; }
         public DbSet<Department> Department { get; set; }
     }
