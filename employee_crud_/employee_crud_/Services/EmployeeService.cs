debb0bab879f1a5fdf823df5116ac9a919c5b565
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 22:32:26 +0530

    Method 02 -> EmployeeService

--- employee_crud_/employee_crud_/Services/EmployeeService.cs
+++ employee_crud_/employee_crud_/Services/EmployeeService.cs
@@ -3,14 +3,7 @@ using employee_crud_.Interfaces.Services;
 
 namespace employee_crud_.Services
 {
-    public class EmployeeService : IEmployeeService
+    public class EmployeeService(IEmployeeService employee) : IEmployeeService
     {
-        private readonly IEmployeeRepository _employee;
-
-        // Constructor injection of IEmployeeRepository
-        public EmployeeService(IEmployeeRepository employee)
-        {
-            _employee = employee;
-        }
     }
 }
