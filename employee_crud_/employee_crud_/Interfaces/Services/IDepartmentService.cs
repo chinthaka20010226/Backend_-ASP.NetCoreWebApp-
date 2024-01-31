e0a9191a10a27b2695ec0f792d4de3aaba8c615e
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 21:51:30 +0530

    Now, DepartmentService Interface is Okay...

--- employee_crud_/employee_crud_/Interfaces/Services/IDepartmentService.cs
+++ employee_crud_/employee_crud_/Interfaces/Services/IDepartmentService.cs
@@ -1,6 +1,13 @@
-﻿namespace employee_crud_.Interfaces.Services
+﻿using employee_crud_.Models.Entities;
+
+namespace employee_crud_.Interfaces.Services
 {
     public interface IDepartmentService
     {
+        Task<Department> GetDepartmentId(int id);
+        Task<IEnumerable<Department>> GetDepartment();
+        Task<Department> InsertDepartment(Department department);
+        Task<Department> UpdateDepartment(Department department);
+        Task<Department> DeleteDepartment(int id);
     }
 }
