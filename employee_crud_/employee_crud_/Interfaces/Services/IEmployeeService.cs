4daf9fbe9a05b3965df1ec985fb6bb11aea2ab3c
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 21:46:23 +0530

    Now, EmployeeService Interface is Okay...

--- employee_crud_/employee_crud_/Interfaces/Services/IEmployeeService.cs
+++ employee_crud_/employee_crud_/Interfaces/Services/IEmployeeService.cs
@@ -1,6 +1,13 @@
-﻿namespace employee_crud_.Interfaces.Services
+﻿using employee_crud_.Models.Entities;
+
+namespace employee_crud_.Interfaces.Services
 {
     public interface IEmployeeService
     {
+        Task<Employee> GetEmployeeId(int id);
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> InsertEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<Employee> DeleteEmployee(int id);
     }
 }
