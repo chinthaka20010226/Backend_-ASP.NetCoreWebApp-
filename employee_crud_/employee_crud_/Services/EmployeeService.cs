445eec0bce4c65bcb222a77ed03633fea22e847c
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 22:31:11 +0530

    Method 01 -> EmployeeService

---
+++ employee_crud_/employee_crud_/Services/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using employee_crud_.Interfaces.Repositories;
+using employee_crud_.Interfaces.Services;
+
+namespace employee_crud_.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employee;
+
+        // Constructor injection of IEmployeeRepository
+        public EmployeeService(IEmployeeRepository employee)
+        {
+            _employee = employee;
+        }
+    }
+}
