5bb482a423746e5c756c8b51a71729468ddf9503
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-31 21:21:26 +0530

    Now, create controllers...

---
+++ employee_crud_/employee_crud_/Controllers/EmployeeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace employee_crud_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+    }
+}
