13472a59f157594bb8ae41cde7cf2a7e611def09
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-01 18:01:49 +0530

    Start the build the Controllers -> Services, Http Methods...

--- employee_crud_/employee_crud_/Controllers/EmployeeController.cs
+++ employee_crud_/employee_crud_/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using employee_crud_.Interfaces.Services;
+using employee_crud_.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace employee_crud_.Controllers
@@ -7,5 +9,30 @@ namespace employee_crud_.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly IEmployeeService _employeeService;
+
+        EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            try
+            {
+                var employee = await _employeeService.GetEmployeeById(id);
+                return Ok(employee);
+            }
+            catch(NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500);
+            }
+        }
     }
 }
