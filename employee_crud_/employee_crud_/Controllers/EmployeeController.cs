369853d3e1a3d0eee8175b8ab37f774b9950b8c2
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-01 18:46:26 +0530

    Http Methods -> EmployeeController...

--- employee_crud_/employee_crud_/Controllers/EmployeeController.cs
+++ employee_crud_/employee_crud_/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using employee_crud_.Interfaces.Services;
+using employee_crud_.Models.Entities;
 using employee_crud_.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,97 @@ namespace employee_crud_.Controllers
             {
                 var employee = await _employeeService.GetEmployeeById(id);
                 return Ok(employee);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            try
+            {
+                var employee = await _employeeService.GetEmployees();
+                return Ok(employee);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertEmployee(Employee employee)
+        {
+            try
+            {
+                var _employee = await _employeeService.InsertEmployee(employee);
+                return Ok(_employee);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateEmployee(Employee employee, int id)
+        {
+            try
+            {
+                if(id <= 0)
+                {
+                    return BadRequest("Id must be greater than 0");
+                }
+
+                if(employee.Id != id)
+                {
+                    return BadRequest("Id in body must match id in rout");
+                }
+
+                var _employee = await _employeeService.UpdateEmployee(employee);
+                return Ok(_employee);
+            }
+            catch(NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            try
+            {
+                if(id <= 0)
+                {
+                    return BadRequest("Id must be greater than 0");
+                }
+
+                var employee = await _employeeService.DeleteEmployee(id);
+                return Ok(employee);
+            }
+            catch (InvalidAgrumentException ex)
+            {
+                return BadRequest(ex.Message);
             }
             catch(NotFoundException ex)
             {
